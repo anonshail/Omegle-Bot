c25d57331644f05e2f49624ba98b3cf9328565e0
Shail K Shah <anonshail@gmail.com>
2020-02-04 19:48:35 +0530

    Omegle Bot Lol

---
+++ README.md
@@ -0,0 +1,5 @@
+OMEGLE BOT!
+
+A simple omegle bot, that invites people with common interest to a discord server
+
+It makes use of the Omegle API pyomegle which can be found here : https://github.com/elias94/pyomegle
---
+++ bot.py
@@ -0,0 +1,64 @@
+from pyomegle import OmegleClient, OmegleHandler
+
+"""
+    Omegle inteface for python
+
+    /next
+        starts a new conversation
+    /exit
+        exits chat session
+"""
+
+interests = ['discord',
+             'furries',
+             'friends',
+             'depressed',
+             'sad',
+             'lgbt',
+             'lgbtq',
+             'emo',
+             'emos',
+             'depression',
+             'lonely',
+             'coffee',
+             'tea',
+             'friend',
+             'reading',
+             'talk',
+             'converation',
+             'friendship',
+             'family',
+             'sadness',
+             'read',
+             'minecraft',
+             'fortnite',
+             'book',
+             'music',
+             'technology',
+             'chill',
+             'song',
+             'songs',
+             'singing']
+
+h = OmegleHandler(loop=True)            # session loop
+c = OmegleClient(h, wpm=47, lang='en', topics=interests)  # 47 words per minute
+c.start()
+
+'''
+while 1:
+    input_str = raw_input('')           # string input
+
+    if input_str.strip() == '/next':
+        c.next()                        # new conversation
+    elif input_str.strip() == '/exit':
+        c.disconnect()                  # disconnect chat session
+        break
+    else:
+        c.send(input_str)               # send string
+'''
+
+while True:
+    input_str = "Hey! We seem to have common interests! Come to our server, and find like minded people! We'd love to see you! Invite: https://discord.gg/vzResA"
+    c.next()
+    c.send(input_str)
+    c.disconnect()
